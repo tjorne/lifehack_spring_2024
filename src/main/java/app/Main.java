52635a5cb4318f4f80d66eb3b60954b8ba0725b9
crstrskp <jesper@tjorne.dk>
2024-03-17 21:44:46 +0100

    init commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,34 @@
+package app;
+
+import app.config.ThymeleafConfig;
+import app.controllers.TimeZonesController;
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main
+{
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "lifehack";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx -> ctx.render("index.html"));
+        UserController.addRoutes(app, connectionPool);
+        TimeZonesController.addRoutes(app, connectionPool);
+    }
+}
---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,71 @@
+package app.persistence;
+
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper
+{
+
+    public static User login(String userName, String password, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "select * from public.\"user\" where user_name=? and password=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        )
+        {
+            ps.setString(1, userName);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+            {
+                int id = rs.getInt("id");
+                String role = rs.getString("role");
+                return new User(id, userName, password, role);
+            } else
+            {
+                throw new DatabaseException("Fejl i login. Prøv igen");
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("DB fejl", e.getMessage());
+        }
+    }
+
+    public static void createuser(String userName, String password, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "insert into users (user_name, password) values (?,?)";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        )
+        {
+            ps.setString(1, userName);
+            ps.setString(2, password);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        }
+        catch (SQLException e)
+        {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value "))
+            {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(msg, e.getMessage());
+        }
+    }
+}
