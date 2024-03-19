2e4f09a780a3461dbda5fe5dc64c42de4d04aabd
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-03-19 12:23:49 +0100

    start page card made

--- src/main/java/app/controllers/UnitConverterController.java
+++ src/main/java/app/controllers/UnitConverterController.java
@@ -8,11 +8,11 @@ public class UnitConverterController
 {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
-        app.get("/UnitConverter", ctx -> index(ctx, connectionPool));
+        app.get("/Gruppe-B8-unit-converter", ctx -> index(ctx, connectionPool));
     }
 
     private static void index(Context ctx, ConnectionPool connectionPool)
     {
-        ctx.render("/UnitConverter/index.html");
+        ctx.render("/Gruppe-B8-unit-converter/index.html");
     }
 }
