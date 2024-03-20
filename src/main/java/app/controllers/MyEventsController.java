1e2f13554712f8f07a699877b5fa241062626df1
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-03-20 18:08:49 +0100

    Implement eventOverview routing

--- src/main/java/app/controllers/MyEventsController.java
+++ src/main/java/app/controllers/MyEventsController.java
@@ -1,6 +1,9 @@
 package app.controllers;
 
+import app.entities.MyEventsEvent;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.MyEventsEventMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,10 +12,31 @@ public class MyEventsController
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
         app.get("/myevents", ctx -> index(ctx, connectionPool));
+        app.get("/myevents/event", ctx -> eventOverview(ctx, connectionPool));
     }
 
     private static void index(Context ctx, ConnectionPool connectionPool)
     {
         ctx.render("/myevents/index.html");
+    }
+
+    private static void eventOverview(Context ctx, ConnectionPool connectionPool) {
+        String eventId = ctx.queryParam("id");
+
+        if (eventId == null) {
+            System.out.println("Error: id query parameter is null.");
+            return;
+        }
+
+        try {
+            MyEventsEvent event = MyEventsEventMapper.getEventById(Integer.parseInt(eventId), connectionPool);
+
+            ctx.attribute("eventItem", event);
+            ctx.render("eventoverview.html");
+        } catch (NumberFormatException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (DatabaseException e) {
+            System.out.println("Database Error: " + e.getMessage());
+        }
     }
 }
