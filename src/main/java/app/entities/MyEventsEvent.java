db0c0dc056c0c058ff0779e754b85de78a792e5b
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-03-21 12:32:51 +0100

    Add city property and override equals and hashCode

--- src/main/java/app/entities/MyEventsEvent.java
+++ src/main/java/app/entities/MyEventsEvent.java
@@ -1,6 +1,7 @@
 package app.entities;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class MyEventsEvent {
     private int id;
@@ -8,16 +9,18 @@ public class MyEventsEvent {
     private LocalDateTime date;
     private String place;
     private int zip;
+    private String city;
     private String resume;
     private String details;
     private String link;
 
-    public MyEventsEvent(int id, String name, LocalDateTime date, String place, int zip, String resume, String details, String link) {
+    public MyEventsEvent(int id, String name, LocalDateTime date, String place, int zip, String city, String resume, String details, String link) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.place = place;
         this.zip = zip;
+        this.city = city;
         this.resume = resume;
         this.details = details;
         this.link = link;
@@ -43,6 +46,10 @@ public class MyEventsEvent {
         return zip;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public String getResume() {
         return resume;
     }
@@ -55,36 +62,17 @@ public class MyEventsEvent {
         return link;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyEventsEvent event = (MyEventsEvent) o;
+        return getId() == event.getId() && getZip() == event.getZip() && Objects.equals(getName(), event.getName()) && Objects.equals(getDate(), event.getDate()) && Objects.equals(getPlace(), event.getPlace()) && Objects.equals(getCity(), event.getCity()) && Objects.equals(getResume(), event.getResume()) && Objects.equals(getDetails(), event.getDetails()) && Objects.equals(getLink(), event.getLink());
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public void setResume(String resume) {
-        this.resume = resume;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDate(), getPlace(), getZip(), getCity(), getResume(), getDetails(), getLink());
     }
 
     @Override
@@ -95,6 +83,7 @@ public class MyEventsEvent {
                 ", date=" + date +
                 ", place='" + place + '\'' +
                 ", zip=" + zip +
+                ", city='" + city + '\'' +
                 ", resume='" + resume + '\'' +
                 ", details='" + details + '\'' +
                 ", link='" + link + '\'' +
