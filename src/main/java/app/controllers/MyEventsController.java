6c1d465b6e140c25d25b6a701bc65c4cdce33484
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-03-21 12:31:13 +0100

    Controller

--- src/main/java/app/controllers/MyEventsController.java
+++ src/main/java/app/controllers/MyEventsController.java
@@ -2,24 +2,60 @@ package app.controllers;
 
 import app.entities.MyEventsCategory;
 import app.entities.MyEventsEvent;
+import app.entities.Task;
+import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.MyEventsEventMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyEventsController
 {
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/myevents", ctx -> index(ctx, connectionPool));
         app.get("/myevents/event", ctx -> eventOverview(ctx, connectionPool));
-        app.post("/myevents/search", ctx -> searchResults(ctx, connectionPool));
 
+        app.post("/myevents/search", ctx -> searchResults(ctx, connectionPool));
+        app.post("/myevents/addtofavorite", ctx -> addToFavorite(ctx, connectionPool));
+        app.post("/myevents/removefromfavorite", ctx -> removeFromFavorite(ctx, connectionPool));
+    }
+
+    private static void removeFromFavorite(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    private static void addToFavorite(Context ctx, ConnectionPool connectionPool) {
+
+        User user = ctx.sessionAttribute("currentUser");
+
+        try {
+            int event_id = Integer.parseInt(ctx.formParam("event_id"));
+            MyEventsEventMapper.addEventToUserFavorites(user.getUserId(), event_id,connectionPool);
+
+            List<MyEventsEvent> eventList = MyEventsEventMapper.getAllEvents(connectionPool);
+            List<MyEventsEvent> userFavoriteEventList = MyEventsEventMapper.getAllUserFavoriteEvents(user.getUserId(), connectionPool);
+
+            Map<MyEventsEvent, Boolean> eventsMap = new LinkedHashMap<>();
+
+            for (MyEventsEvent event : eventList) {
+                if (userFavoriteEventList.contains(event)) {
+                    eventsMap.put(event, true);
+                } else {
+                    eventsMap.put(event, false);
+                }
+            }
+
+            ctx.attribute("eventMap", eventsMap);
+            //ctx.attribute("eventList", eventList);
+            ctx.render("favorites.html");
+
+        } catch (DatabaseException e) {
+            ctx.attribute("message",e.getMessage());
+            ctx.render("/myevents/index.html");
+        }
     }
 
     private static void searchResults(Context ctx, ConnectionPool connectionPool) {
