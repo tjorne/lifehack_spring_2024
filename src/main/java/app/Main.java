e853357f4f90b3f3336a6885c25be21b86e3c034
MateenCPH <cph-mr645@cphbusiness.dk>
2024-03-21 10:52:41 +0100

    update of main routing

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import app.config.ThymeleafConfig;
+import app.controllers.QuotesGeneratorController;
 import app.controllers.TimeZonesController;
 import app.controllers.UserController;
 import app.persistence.ConnectionPool;
@@ -30,5 +31,6 @@ public class Main
         app.get("/", ctx -> ctx.render("index.html"));
         UserController.addRoutes(app, connectionPool);
         TimeZonesController.addRoutes(app, connectionPool);
+        QuotesGeneratorController.addRoutes(app, connectionPool);
     }
 }
